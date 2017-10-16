cec70a046f0289f859883fb36970f0da41513cec
laifulin <laifulin@32gold.com>
2017-10-16 10:35:12 +0800

    Initial commit

---
+++ xiaoqingxin-server/src/main/java/com/xiaoqingxin/server/XiaoqingxinServerApplication.java
@@ -0,0 +1,12 @@
+package com.xiaoqingxin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoqingxinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoqingxinServerApplication.class, args);
+	}
+}
